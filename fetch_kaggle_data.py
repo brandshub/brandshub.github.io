215c5e01ee508227c64ef6475663d0b3c8142ee1
Serhiy Pavlov <sergey.pavlov@voliasoftware.com>
2025-07-22 00:58:52 +0300

    should be it

--- fetch_kaggle_data.py
+++ fetch_kaggle_data.py
@@ -8,10 +8,10 @@ api.authenticate()
 api.dataset_download_files('piterfm/massive-missile-attacks-on-ukraine', unzip=True)
 
 df = pd.read_csv('missile_attacks_daily.csv')  # Adjust filename as needed
-latest = df.sort_values('time_end').tail(1).to_dict(orient='records')[0]
-
+records = df.to_dict(orient='records')
 
 os.makedirs('mdata', exist_ok=True)
 
+
 with open('mdata/kaggle_missile_attacks_daily.json', 'w') as f:
-    json.dump(latest, f, indent=2)
+    json.dump(records, f, indent=2)
