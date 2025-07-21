f23984dd5164100fa9275b872c090f6eff598bfc
Serhiy Pavlov <sergey.pavlov@voliasoftware.com>
2025-07-22 00:39:10 +0300

    action to get missile data?

---
+++ fetch_kaggle_data.py
@@ -0,0 +1,17 @@
+import json
+import os
+import pandas as pd
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+api = KaggleApi()
+api.authenticate()
+api.dataset_download_files('piterfm/massive-missile-attacks-on-ukraine', unzip=True)
+
+df = pd.read_csv('missile_attacks_daily.csv')  # Adjust filename as needed
+latest = df.sort_values('date').tail(1).to_dict(orient='records')[0]
+
+
+os.makedirs('mdata', exist_ok=True)
+
+with open('mdata/kaggle_missile_attacks_daily.json', 'w') as f:
+    json.dump(latest, f, indent=2)
